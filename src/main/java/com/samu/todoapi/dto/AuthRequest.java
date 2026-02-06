a0a4542cd4dfbbb18b94f702e152f9edda5eb880
Samuel Dias Oliveira <samueldias004@gmail.com>
2026-02-05 22:17:06 -0300

    feat: starting auth service

---
+++ src/main/java/com/samu/todoapi/dto/AuthRequest.java
@@ -0,0 +1,3 @@
+package com.samu.todoapi.dto;
+
+public record AuthRequest(String username, String password) {}
--- src/main/java/com/samu/todoapi/service/UserService.java
+++ src/main/java/com/samu/todoapi/service/UserService.java
@@ -5,6 +5,7 @@ import com.samu.todoapi.dto.UserDetailsDTO;
 import com.samu.todoapi.entity.User;
 import com.samu.todoapi.mapper.UserMapper;
 import com.samu.todoapi.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,14 +14,17 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public UserCreateDTO create(UserCreateDTO taskDTO) {
-        User newUser = userRepository.save(userMapper.toEntity(taskDTO));
+    public UserCreateDTO create(UserCreateDTO userDTO) {
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+        User newUser = userRepository.save(userMapper.toEntity(userDTO));
         return userMapper.toCreateDTO(newUser);
     }
 
